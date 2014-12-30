8776fe036c7d9a101158d081caf7eaefb78201e6
valkyrie92 <petru.office92@gmail.com>
2014-12-30 16:16:12 +0200

    Updated ramMemory() for BSD systems

--- app/boot/Remote.class.php
+++ app/boot/Remote.class.php
@@ -70,11 +70,32 @@
 			case 'Linux':
 				$used = $this->ssh->exec('free | grep Mem | awk \'{print $3/$2 * 100.0}\'');
 				$free = $this->ssh->exec('free | grep Mem | awk \'{print $4/$2 * 100.0}\'');
-				$total = $this->ssh->exec('free | grep Mem | awk \'{print $1}\'');
-				return $used . '/' . $free . ' din ' . $total;
+				$total = $this->ssh->exec('free | grep Mem | awk \'{print $1/1024.0*1024.0}\'');
+				return $used . '/' . $free . ' din ' . $total . ' MB';
 				break;
 			case 'FreeBSD':
-				return false;
+				$mem_hw 		= mem_rounded($this->ssh->exec('sysctl hw.physmem'));
+				$mem_phys 		= $this->ssh->exec('sysctl hw.physmem');
+				$mem_all 		= $this->ssh->exec('sysctl wm.stats.vm.v_page_count') 		* $this->ssh->exec('sysctl hw.pagesize');
+				$mem_wire 		= $this->ssh->exec('sysctl wm.stats.vm.v_wire_count') 		* $this->ssh->exec('sysctl hw.pagesize');
+				$mem_active 	= $this->ssh->exec('sysctl vm.stats.vm.v_active_count') 	* $this->ssh->exec('sysctl hw.pagesize');
+				$mem_inactive 	= $this->ssh->exec('sysctl vm.stats.vm.v_inactive_count') 	* $this->ssh->exec('sysctl hw.pagesize');
+				$mem_cache 		= $this->ssh->exec('sysctl vm.stats.vm_v_cache_count') 		* $this->ssh->exec('sysctl hw.pagesize');
+				$mem_free 		= $this->ssh->exec('sysctl vm.stats.vm_v_free_count') 		* $this->ssh->exec('sysctl hw.pagesize');
+
+				$mem_gap_vm    = $mem_all - ($mem_wire + $mem_active + $mem_inactive + $mem_cache + $mem_free);
+				$mem_gap_sys   = $mem_phys - $mem_all;
+				$mem_gap_hw    = $mem_hw   - $mem_phys;
+
+				$mem_total = $mem_hw;
+				$mem_avail = $mem_inactive + $mem_cache + $mem_free;
+				$mem_used  = $mem_total - $mem_avail;
+				
+				$free = ($mem_inactive + $mem_cache + $mem_free)/$mem_total*100.0;
+				$used  = ($mem_total - $mem_avail)/$mem_total*100.0;
+				$total = $mem_total/1024*1024;
+
+				return $used . '/' . $free . ' din ' . $total . ' MB';
 				break;
 			default:
 				break;
@@ -83,6 +104,5 @@
 
 	public function cpuUsage() {
 		
-	}
 	}
  }
--- app/boot/functions.php
+++ app/boot/functions.php
@@ -16,3 +16,15 @@ function server_ip($username, $DB) {
 	$data = $DB->query("SELECT server_ip FROM lcpc_clients WHERE username='{$username}'")->fetch_row();
 	return $data[0];
 }
+
+// function taken from freebsd-memory -- List Total System Memory Usage
+function mem_rounded($mem_size) {
+	$chip_size = 1;
+	$chip_guess = ($mem_size/8)-1;
+	while($chip_guess != 0) {
+		$chip_guess += 1;
+		$chip_size -= 1;
+	}
+	$mem_round = ((int) ($mem_size / $chip_size) +1) * $chip_size;
+	return $mem_round;
+}
