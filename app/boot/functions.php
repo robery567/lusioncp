ef32874e6fb7dd3b3c0d251659701b13ffad2224
robery567 <robery_office@yahoo.ro>
2014-12-31 15:29:03 +0200

    Added new functions

--- app/boot/functions.php
+++ app/boot/functions.php
@@ -21,3 +21,20 @@ function is_installed($ip, $DB) {
     $data = $DB->query("SELECT is_installed AS response FROM lcpc_clients")->fetch_array(MYSQLI_ASSOC);
     return $data['response'];
 }
+
+function is_ipinstalled($ip, $DB) {
+    $data = $DB->query("SELECT is_installed AS response FROM lcpc_clients WHERE server_ip='{$ip}'")->fetch_array(MYSQLI_ASSOC);
+    if (is_null($data['response'])) {
+        return 1;
+    } else {
+        return $data['response'];
+    }
+}
+
+function update_status($ip, $status) { // @PARAM : IP (SERVER_IP), STATUS(0/1)
+    $DB->query("UPDATE lcpc_clients SET is_installed=1 WHERE server_ip='{$ip}'");
+}
+
+function update_procent($ip, $procent) {
+    $DB->query("UPDATE lcpc_clients SET install_procent='{$procent}' WHERE server_ip='{$ip}'");
+}
