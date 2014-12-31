22bed6db7ea1aaa71e8100c9a8e58977bf79d5e5
valkyrie92 <petru.office92@gmail.com>
2014-12-31 14:57:32 +0200

    Added client_install.php

---
+++ web/client_install.php
@@ -0,0 +1,30 @@
+<?php
+	error_reporting(0);
+	require __DIR__ . '/../app/boot/start.php';
+	
+	if (is_ipinstalled($_SERVER['REMOTE_ADDR'], $DB)) {
+		redirect('index.php');
+	}
+	
+	$action  = isset($_GET['action'])  ? $_GET['action']  : NULL;
+	$procent = isset($_GET['procent']) ? $_GET['procent'] : NULL;
+	
+	if (is_null($action)) {
+		redirect('index.php');
+	}
+	
+	switch ($action) {
+		case 'install_procent':
+			if (is_null($procent)) {
+				redirect('index.php');
+			} else {
+				update_procent($_SERVER['REMOTE_ADDR'], $procent);
+			}
+		break;
+		
+		case 'update_status':
+			update_status($_SERVER['REMOTE_ADDR'], 1); // INSTALLED
+		break;
+	
+	}
+?>
