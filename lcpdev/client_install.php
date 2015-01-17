ff3f3b45029e8b416985fddcd389ecaca2039cac
Petru <petru.office92@gmail.com>
2015-01-17 22:35:46 +0200

    Removed comment

--- lcpdev/client_install.php
+++ lcpdev/client_install.php
@@ -1,18 +1,18 @@
 <?php
 
 	require __DIR__ . '/../../private/app/boot/start.php';
-	
+
 	if (is_ipinstalled($_SERVER['REMOTE_ADDR'])) {
 		redirect('index.php');
 	}
-	
+
 	$action  = isset($_GET['action'])  ? $_GET['action']  : NULL;
 	$procent = isset($_GET['procent']) ? $_GET['procent'] : NULL;
-	
+
 	if (is_null($action)) {
 		redirect('index.php');
 	}
-	
+
 	switch ($action) {
 		case 'install_procent':
 			if (is_null($procent)) {
@@ -21,10 +21,10 @@
 				update_procent($_SERVER['REMOTE_ADDR'], $procent);
 			}
 		break;
-		
+
 		case 'update_status':
-			update_status($_SERVER['REMOTE_ADDR'], 1); // INSTALLED
+			update_status($_SERVER['REMOTE_ADDR'], 1);
 		break;
-	
+
 	}
 ?>
