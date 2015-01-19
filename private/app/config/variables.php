a7c323ec0c9a07f2c137aac7fd377f1ab038988a
Petru <petru.office92@gmail.com>
2015-01-19 11:44:17 +0200

    Added debug mode

--- private/app/config/variables.php
+++ private/app/config/variables.php
@@ -1,6 +1,7 @@
 <?php
 
 $config = [
+  'debug' => true,
   'session_savepath' => __DIR__ . '/../safelocker/sessions/',
   'default_timezone' => 'Europe/Bucharest', // http://php.net/manual/en/timezones.php
 ];
