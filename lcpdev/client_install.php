a855892466fd6a09e0f89ead72513d78711e6f5f
Petru <petru.office92@gmail.com>
2015-01-21 23:12:03 +0200

    Install script secures mysql root user

--- lcpdev/client_install.php
+++ lcpdev/client_install.php
@@ -22,6 +22,12 @@
 			}
 		break;
 
+		case 'get_random_password':
+			$salt = 'r3r2r98gfe89rg8eawg4';
+			$hash = hash('md5', str_rot13($salt.date('d-m-Y H:i:s').mt_rand(0, mt_getrandmax())))."\n";
+			echo $hash;
+		break;
+
 		case 'update_status':
 			update_status($_SERVER['REMOTE_ADDR'], 1);
 		break;
