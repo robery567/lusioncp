77af6c1ddca9cdf6d6113b5f6dd769e453bc8b0d
Petru <petru.office92@gmail.com>
2015-01-24 20:50:37 +0200

    Added sync_key action

--- lcpmain/apiservice.php
+++ lcpmain/apiservice.php
@@ -3,15 +3,15 @@
 require __DIR__ . '/../../private/app/boot/functions.php';
 
 try {
-			$db2 = new mysqli('localhost', 'lcp_adevel', 'RxxlRN1yCy', 'lcp_admin');
+	$db2 = new mysqli('localhost', 'lcp_adevel', 'RxxlRN1yCy', 'lcp_admin');
 
-			if ($db2->connect_error) {
-				throw new Exception('CONNECT_PROBLEM');
-			} else {
-				if($db2->query("SELECT COUNT(DISTINCT `table_name`) FROM `information_schema`.`columns` WHERE `table_schema` = 'lcp_admin'")->num_rows == 0) {
-					throw new Exception('CORRUPT_DATABASE');
-				}
-			}
+	if ($db2->connect_error) {
+		throw new Exception('CONNECT_PROBLEM');
+	} else {
+		if($db2->query("SELECT COUNT(DISTINCT `table_name`) FROM `information_schema`.`columns` WHERE `table_schema` = 'lcp_admin'")->num_rows == 0) {
+			throw new Exception('CORRUPT_DATABASE');
+		}
+	}
 } catch (Exception $e) {
 	switch($e->getMessage()) {
 		case 'CONNECT_PROBLEM':
@@ -26,6 +26,31 @@ try {
 			echo '__NECUNOSCUT__';
 		break;
 	}
+}
+
+function strrand($string) {
+	$letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	$rand_n = mt_rand(1, 26);
+	$number = $rand_n % 26;
+
+	if(!$n) {
+		return $string;
+	}
+
+	if ($n == 13) {
+		return str_rot13($string);
+	}
+
+	for ($i = 0; $i < strlen($string); $i++) {
+		$char = $string[$i];
+		if ($char >= 'a' && $char <= 'z') {
+			$string[$i] = $letters[(ord($char) - 71 + $n) % 26];
+		} else if ($char >= 'A' && $char <= 'Z') {
+			$string[$i] = $letters[(ord($char) - 39 + $n) % 26 + 26];
+		}
+	}
+
+	return $string;
 }
 
 $action = isset($_GET['action']) ? sanitize($_GET['action']) : 'invalid';
@@ -209,6 +234,92 @@ switch($action) {
       echo 'invalid account';
     }
 	break;
+
+	case 'keygen':
+		$options = [
+			'salt' => mcrypt_create_iv(22, MCRYPT_DEV_URANDOM),
+		];
+
+		echo hash('sha512', strrand(hash('md5', $salt)));
+		break;
+
+	case 'sync_license':
+		$get_sync = isset($_GET['sync_key']) ? sanitize($_GET['sync_key']) : 'invalid';
+		$query = "
+			SELECT
+				`license_ip`,
+				`license_key`,
+				`license_startdate`,
+				`license_expiry`,
+				`available`
+			FROM
+				`lcpa_clients`
+			WHERE
+				`license_ip` = '{$get_ip}'
+				AND
+					`license_key` = '{$get_key}'
+		";
+		$check_key = $db2->query($query);
+		$client = $check_key->fetch_array(MYSQLI_ASSOC);
+
+		if($check_key->num_rows == 1) {
+			if($client['available'] == 0) {
+				if($client['license_key'] == $get_key) {
+					if($client['license_ip'] == $get_ip) {
+						$query = "
+							SELECT
+								`lcpa_clients`.`user_id` AS `company`
+							FROM
+								`lcpa_clients`
+							INNER JOIN
+								`lcpa_license`
+								ON
+									`lcpa_clients`.`user_id` = `lcpa_license`.`company_id`
+							WHERE
+								`lcpa_license`.`license_ip` = '{$get_ip}'
+								AND
+									`lcpa_license`.`license_key` = '{$get_key}'
+
+						";
+						$check = $db2->query($query);
+						if ($check->num_rows == 1) {
+							$row = $check->fetch_object();
+							$query = "
+								INSERT INTO
+									`lcpa_license` (
+										`company_id`,
+										`company_parent`,
+										`license_key`,
+										`available`
+									) VALUES (
+										'{$row->company}',
+										'{$row->company}',
+										'{$get_sync}',
+										'1'
+									)
+							";
+							$send = $db2->query($query);
+							if ($send->insert_id) {
+								echo 'sync ok';
+							} else {
+								echo 'sync fail';
+							}
+						} else {
+							echo 'invalid company';
+						}
+					} else {
+						echo 'invalid ip';
+					}
+				} else {
+					echo 'invalid license key';
+				}
+			} else {
+				echo 'invalid expired';
+			}
+		} else {
+			echo 'invalid license';
+		}
+		break;
 
 	default:
 		echo 'invalid action';
